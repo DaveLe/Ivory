fc1faa319a06a9bb78196e26009a030a7b0a1273
David Le <David@Davids-MacBook-Air.local>
2018-02-03 15:08:18 -0500

    Updated

---
+++ Ivory/app/src/main/java/com/iter/ivory/VaccineInfo.java
@@ -0,0 +1,22 @@
+package com.iter.ivory;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class VaccineInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vaccine_info_layout);
+
+        TextView vaccination = findViewById(R.id.vac);
+
+    }
+
+}
