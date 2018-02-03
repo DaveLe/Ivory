22e90a6283f357f4a3b89993b86f39121c260e09
David Margolin <davidmargolin0305@gmail.com>
2018-02-03 18:52:24 -0500

    submission

--- Ivory/app/src/main/java/com/iter/ivory/AddVaccination.java
+++ Ivory/app/src/main/java/com/iter/ivory/AddVaccination.java
@@ -9,13 +9,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class AddVaccination extends AppCompatActivity {
-
+    ArrayAdapter<CharSequence> adapter;
     ArrayList<String> sublist = new ArrayList<>();
     FirebaseAuth authUser = FirebaseAuth.getInstance();
     User u = new User(authUser.getCurrentUser().getDisplayName(),new ArrayList<Vaccines>());
@@ -26,11 +29,17 @@ public class AddVaccination extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_vaccination);
 
+        DocumentReference userinfo;
+        userinfo = db.collection("users").document(authUser.getUid());
+        userinfo.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    u = documentSnapshot.toObject(User.class);
+            }
+        });
         Spinner vaccinationList = findViewById(R.id.spinner);
         final Spinner vaccinationsubList = findViewById(R.id.spinnersub);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.Adenovirus));
+        adapter = ArrayAdapter.createFromResource(getApplication(), R.array.Anthrax, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         vaccinationsubList.setAdapter(adapter);
 
@@ -45,7 +54,7 @@ public class AddVaccination extends AppCompatActivity {
                 int yearNumber = Integer.valueOf(yearInput);
                 String monthInput = monthView.getText().toString();
                 int monthNumber = Integer.valueOf(monthInput);
-                Vaccines vac = new Vaccines("To - Do", "subthing", monthNumber, yearNumber);
+                Vaccines vac = new Vaccines("Anthrax", "AVA (BioThrax)", monthNumber, yearNumber);
                 u.addVaccinations(vac);
                 db.collection("users").document(authUser.getUid()).set(u);
                 finish();
@@ -55,22 +64,12 @@ public class AddVaccination extends AppCompatActivity {
         vaccinationList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                ArrayAdapter<String> adapter;
                 switch(position){
 
                     case 1:
-                        adapter = new ArrayAdapter<String>(getApplication(),
-                                android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.Anthrax));
-                    default:
-                        adapter = new ArrayAdapter<String>(getApplication(),
-                                android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.Adenovirus));
-
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    vaccinationsubList.setAdapter(adapter);
+                        adapter = ArrayAdapter.createFromResource(getApplication(), R.array.Anthrax, android.R.layout.simple_spinner_item);
+                        adapter.notifyDataSetChanged();
                 }
-
-
-
 
             }
 
--- Ivory/app/src/main/java/com/iter/ivory/MainActivity.kt
+++ Ivory/app/src/main/java/com/iter/ivory/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,18 +21,24 @@ private val providers = mutableListOf(
 
 class MainActivity : AppCompatActivity(), VaccineFragment.OnListFragmentInteractionListener{
 
+    var bottomnav : BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 //        val name : Vaccines = Vaccines("HPV2", Date())
 //        Log.e("hello",name.startDate.toString())
 //        Log.e("TIME",Vaccines.calculateRemindDate(Date(),4,1).toString() )
+        setContentView(R.layout.activity_main)
+        // setup bottom navigation bar
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
             // if user is already signed in get name and uid
             val uid = user.uid
             val name = user.displayName
+            bottomnav = navigation
+            bottomnav!!.selectedItemId = R.id.navigation_personal
 
         }else{
             // handle sign in
@@ -42,10 +51,7 @@ class MainActivity : AppCompatActivity(), VaccineFragment.OnListFragmentInteract
 
         }
 
-        setContentView(R.layout.activity_main)
-        // setup bottom navigation bar
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        navigation.selectedItemId = R.id.navigation_personal
+
     }
 
 
@@ -58,11 +64,15 @@ class MainActivity : AppCompatActivity(), VaccineFragment.OnListFragmentInteract
                 val user = FirebaseAuth.getInstance().currentUser!!
                 val uid = user.uid
                 val name = user.displayName
-
+                if (bottomnav != null){
+                    bottomnav!!.selectedItemId = R.id.navigation_personal
+                }
             } else {
                 // TODO: handle sign in failed
             }
         }else if (requestCode == 5){
+            bottomnav!!.selectedItemId = R.id.navigation_personal
+
             if (resultCode == RESULT_OK) {
                 // Successfully signed in - update the user object
                 val name = data!!.getStringExtra("name")
@@ -78,7 +88,7 @@ class MainActivity : AppCompatActivity(), VaccineFragment.OnListFragmentInteract
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            // TODO: decide on which navigation menus to include
+        // TODO: decide on which navigation menus to include
             R.id.navigation_personal -> {
                 val vaccineFragment: Fragment = VaccineFragment.newInstance(true)
                 supportFragmentManager.beginTransaction().replace(R.id.content_view, vaccineFragment).commit()
@@ -94,7 +104,27 @@ class MainActivity : AppCompatActivity(), VaccineFragment.OnListFragmentInteract
     }
 
     override fun onListFragmentInteraction(new: Boolean) {
-        val newAdder = Intent(this, AddVaccination::class.java)
+        val newAdder = Intent(this, VaccineInfo::class.java)
         startActivityForResult(newAdder, 5)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = getMenuInflater()
+        inflater.inflate(R.menu.main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.add -> {
+                val newAdder = Intent(this, AddVaccination::class.java)
+                startActivityForResult(newAdder, 5)
+            }
+            else -> {
+                return true;
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
