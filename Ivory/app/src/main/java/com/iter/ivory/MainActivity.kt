f332587ae490d59e132aff07eeedae6321d75f0e
David Margolin <davidmargolin0305@gmail.com>
2018-02-02 21:33:41 -0500

    added phone and email user authorization via firebase

--- Ivory/app/src/main/java/com/iter/ivory/MainActivity.kt
+++ Ivory/app/src/main/java/com/iter/ivory/MainActivity.kt
@@ -1,14 +1,68 @@
 package com.iter.ivory
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+
+
+// sign in response id and providers used with AuthUI
+private const val RC_SIGN_IN = 420
+private val providers = mutableListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.PhoneBuilder().build())
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            // if user is already signed in get name and uid
+            val uid = user.uid
+            val name = user.displayName
+
+        }else{
+            // handle sign in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN)
+
+        }
+
+        setContentView(R.layout.activity_main)
+        // setup bottom navigation bar
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in - update the user object
+                val user = FirebaseAuth.getInstance().currentUser!!
+                val uid = user.uid
+                val name = user.displayName
+
+            } else {
+                // TODO: handle sign in failed
+            }
+        }
+    }
+
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
+            // TODO: decide on which navigation menus to include
             R.id.navigation_home -> {
                 message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
@@ -23,12 +77,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
         false
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
